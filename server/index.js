65612ac0c960a68834d5240f3d162d010baa1445
Eduardo Juarez <eduardo._.juarez@live.com.ar>
2021-11-27 19:36:52 -0300

    api completa

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
+
 
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
+app.use(cors());
 
 //MySQL
 const pool = mysql.createPool({
@@ -16,6 +18,124 @@ const pool = mysql.createPool({
     password: '',
     database: 'capacitacionesdb'
 });
+
+
+//CAPACITACIONES CONECCIONES
+//POST CAPACITACIONES
+app.post('/capacitaciones/nuevo', (req, res) => {
+    pool.getConnection((err, connection) => {
+        //res.header("Access-Control-Allow-Origin", "*")
+
+        const params = req.body
+        connection.query('INSERT INTO capacitaciones SET ?', params, (err, rows) => {
+            connection.release() //devuelve la conecction a la pool
+
+            if (!err) {
+                res.send(`la capacitacion ${params.nombre} ha sido agregada correctamente.`)
+                res.redirect('/capacitacion')
+            } else {
+                console.log(err)
+            }
+        })
+    })
+});
+
+
+//get todas capacitaciones
+app.get('/capacitaciones', (req, res)=> {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        //query(sqlString, callback)
+        connection.query('SELECT * from capacitaciones',(err, rows) => {
+            connection.release() //devuelve la conecction a la pool
+
+            if (!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+        })
+    })
+});
+
+//Delete capacitaciones
+app.delete('/capacitaciones/:idcapacitaciones/delete', (req, res)=> {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        //query(sqlString, callback)
+        connection.query('DELETE from capacitaciones WHERE idcapacitacion = ?', [req.params.idcapacitaciones], (err, rows) => {
+            connection.release() //devuelve la conecction a la pool
+
+            if (!err) {
+                res.send(`Capacitacion con el ID: ${[req.params.idcapacitaciones]} ha sido eliminada`)
+                res.redirect('/home');
+            } else {
+                console.log(err)
+            }
+        })
+    })
+});
+//get capacitaciones by ID
+app.get('/capacitaciones/:idcapacitaciones', (req, res)=> {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        //query(sqlString, callback)
+        connection.query('SELECT * from capacitaciones WHERE idcapacitacion = ?', [req.params.idcapacitaciones], (err, rows) => {
+            connection.release() //devuelve la conecction a la pool
+
+            if (!err) {
+                res.send(rows)
+                console.log(`enviado usuario de id ${req.params.idcapacitaciones}`)
+            } else {
+                console.log(err)
+            }
+        })
+    })
+});
+
+//Edit/update de capacitaciones
+app.put(`/capacitaciones/:idcapacitacion/edit`, (req, res)=> {
+    pool.getConnection((err, connection) => {
+        if(err) throw errcd
+        console.log(`connected as id ${connection.threadId}`)
+
+        //query(sqlString, callback)
+        const {
+            nombre,
+            temario,
+            tipo,
+            certificacion,
+            fecha,
+            plan,
+            material,
+            observaciones,
+            invitados
+        } = req.body
+
+        connection.query('UPDATE capacitaciones SET nombre = ?, temario = ?, tipo = ?, certificacion = ?, fecha = ?, plan = ?, material = ?, observaciones = ?, invitados = ? WHERE idcapacitaciones = ?',
+        [nombre, temario, tipo, certificacion, fecha, plan, material, observaciones, invitados], (err, rows) => {
+            connection.release() //devuelve la conecction a la pool
+
+            if (!err) {
+                res.send(` La capacitacion ${nombre} ha sido modificada.`)
+                res.redirect('/capacitaciones');
+            } else {
+                console.log(err)
+            }
+        })
+
+        console.log(req.body)
+    })
+});
+
+
+//ASISTENTES CONECCIONES
 
 //get rows
 app.get('/asistente', (req, res)=> {
@@ -56,10 +176,10 @@ app.get('/asistente/:id', (req, res)=> {
 });
 
 //Delete asistente
-app.delete('/asistente/:id', (req, res)=> {
+app.delete('/asistente/:idasistente/delete', (req, res)=> {
     pool.getConnection((err, connection) => {
         if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+        console.log(`connected to delete as id ${connection.threadId}`)
 
         //query(sqlString, callback)
         connection.query('DELETE from asistentes WHERE idasistente = ?', [req.params.id], (err, rows) => {
@@ -76,9 +196,10 @@ app.delete('/asistente/:id', (req, res)=> {
 
 //Add an asistentes
 app.post('/asistente/nuevo', (req, res)=> {
+    res.header("Access-Control-Allow-Origin", "*")
     pool.getConnection((err, connection) => {
         if(err) throw err
-        console.log(`connected as id ${connection.threadId}`)
+        console.log(`connected to add as id ${connection.threadId}`)
 
         //query(sqlString, callback)
         const params = req.body
@@ -94,13 +215,13 @@ app.post('/asistente/nuevo', (req, res)=> {
 
         console.log(req.body)
     })
-})
+});
 
 //Edit/update an asistente
-app.put('/asistente', (req, res)=> {
+app.put('/asistente/:idasistente/edit', (req, res)=> {
     pool.getConnection((err, connection) => {
         if(err) throw errcd
-        console.log(`connected as id ${connection.threadId}`)
+        console.log(`connected to edit as id ${connection.threadId}`)
 
         //query(sqlString, callback)
         const {
@@ -127,11 +248,11 @@ app.put('/asistente', (req, res)=> {
 
         console.log(req.body)
     })
-})
+});
 
 
 //se activa puerto en puerto especifico
 const PORT = 3006
 app.listen(PORT, () => {
-   console.log(`Corrriendo en ${PORT}`); 
-});
+   console.log(`Corriendo en ${PORT}`) 
+})
