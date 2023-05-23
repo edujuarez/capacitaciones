464bf905ac5c7a0a18cc8431d28fe5e4516fac6f
eduardo juarez <eduardo._.juarez@live.com.ar>
2023-05-23 15:55:00 -0300

    empezando la auth de firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import React from 'react';
+import firebase from 'firebase/compat/app';
 
 import Layout from './Layout';
 
@@ -10,7 +11,6 @@ import CapacitacionView from './capacitacionView';
 import Capacitacion from './Capacitacion';
 import AsistenteView from './AsistenteView';
 import AsistenteEdit from './AsistenteEdit';
-import Login from './Login';
 import AddAsistentes from './AddAsistentes';
 import Search from './Search';
 import Certification from './certification';
@@ -19,40 +19,54 @@ import Informes from './Informes';
 import Historial from './Historial';
 import Calificaciones from './Calificaciones';
 import CapacitacionEdit from './CapacitacionEdit';
+import Login from './Login';
 
 function App() {
   const user = 'Usuario';
+
+  const isLoggedIn = () => {
+    // Aquí debes implementar tu lógica para verificar si el usuario ha iniciado sesión.
+    // Por ejemplo, puedes utilizar el estado local o el contexto para almacenar el estado de autenticación.
+    // Devuelve true si el usuario ha iniciado sesión, de lo contrario, devuelve false.
+    // Puedes reemplazar este código con tu propia lógica de verificación de inicio de sesión.
+    const user = firebase.auth().currentUser;
+    return !!user;
+  }
+
   return (
-    <>
-      <BrowserRouter>
+    <withRouter>
+      <Switch>
         <Route exact path='/login' component={Login} />
-      </BrowserRouter>
+        <Route
+          render={() =>
+            isLoggedIn() ? (
 
-      <BrowserRouter>
-
-        <Layout user={user}>
-
-          <Switch>
-            <Route exact path="/home" component={Capacitacion} />
-            <Route exact path="/nuevacapacitacion" component={NuevaCapacitacionForm} />
-            <Route exact path="/nuevoasistente" component={NuevoAsistente} />
-            <Route exact path="/asistentes" component={AsistentesList} />
-            <Route exact path="/capacitacion" component={Capacitacion} />
-            <Route exact path="/capacitaciones/:idcapacitacion" component={CapacitacionView} />
-            <Route exact path='/asistentes/:idasistente' component={AsistenteView} />
-            <Route exact path='/asistentes/:idasistente/edit' component={AsistenteEdit} />
-            <Route exact path="/addasistentes/:idcapacitacion" component={AddAsistentes} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/informes" component={Informes} />
-            <Route exact path="/asistencia/:idasistencia" component={PuntajeEdit} />
-            <Route exact path="/historial" component={Historial} />
-            <Route exact path="/certification/:idcapacitacion" component={Certification} />
-            <Route exact path="/calificaciones/:idcapacitacion" component={Calificaciones} />
-            <Route exact path="/capacitaciones/:idcapacitacion/edit" component={CapacitacionEdit} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </>
+              <Layout user={user}>
+                <Route exact path="/" component={Capacitacion} />
+                <Route exact path="/home" component={Capacitacion} />
+                <Route exact path="/nuevacapacitacion" component={NuevaCapacitacionForm} />
+                <Route exact path="/nuevoasistente" component={NuevoAsistente} />
+                <Route exact path="/asistentes" component={AsistentesList} />
+                <Route exact path="/capacitacion" component={Capacitacion} />
+                <Route exact path="/capacitaciones/:idcapacitacion" component={CapacitacionView} />
+                <Route exact path='/asistentes/:idasistente' component={AsistenteView} />
+                <Route exact path='/asistentes/:idasistente/edit' component={AsistenteEdit} />
+                <Route exact path="/addasistentes/:idcapacitacion" component={AddAsistentes} />
+                <Route exact path="/search" component={Search} />
+                <Route exact path="/informes" component={Informes} />
+                <Route exact path="/asistencia/:idasistencia" component={PuntajeEdit} />
+                <Route exact path="/historial" component={Historial} />
+                <Route exact path="/certification/:idcapacitacion" component={Certification} />
+                <Route exact path="/calificaciones/:idcapacitacion" component={Calificaciones} />
+                <Route exact path="/capacitaciones/:idcapacitacion/edit" component={CapacitacionEdit} />
+              </Layout>
+            ) : (
+              <Redirect to='/login' />
+            )
+          }
+        />
+      </Switch>
+    </withRouter>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import login from './styles/login.css'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
 
-class Login extends React.Component {
-    render() {
-        return (
-            <div className="loginBackground">
-                <h1>
-                    Base Capacitaciones holu
-                    
-                </h1>
-            </div>
 
-        )
-    }
-}
-export default Login
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password);
+            history.push('/'); // Ruta a la página de dashboard o cualquier otra página después de iniciar sesión
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
