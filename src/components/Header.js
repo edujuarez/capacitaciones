60e8abf647555b858ae66f788bbc76aaeea3eab5
eduardo juarez <eduardo._.juarez@live.com.ar>
2023-05-29 15:59:37 -0300

    add google auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './styles/header.css';
 import { IoMdExit } from 'react-icons/io';
 import logo from '../images/Logo.png';
+import { Redirect } from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
+        const logout = () => {
+            localStorage.clear()
+
+        }
         return (
             <>
                 <div className="mainHeader">
@@ -14,7 +19,7 @@ class Header extends React.Component {
                     <div className='title'>
                         <h1>Mentor Iselín</h1>
                     </div>
-                    <a href="#" className='exitContainer'>
+                    <button className='exitContainer' onClick={logout}>
                         <div>
                             <h1>
                                 Salir
@@ -23,9 +28,9 @@ class Header extends React.Component {
                         <div className='iconContainer'>
                             <IoMdExit className='iconExit' />
                         </div>
-                    </a>
+                    </button>
                 </div>
-                <div className="mainHeader">
+                <div className="mainHeader2">
                     <a href="/capacitacion"><h1>Capacitaciones</h1></a>
                     <a href="/asistentes"><h1>Asistentes</h1></a>
                     <a href="/informes"><h1>Informe de capacitación</h1></a>
