9849af84d7bda88a1fa58f94dc7f2dc121b176c9
eduardo juarez <eduardo._.juarez@live.com.ar>
2023-06-05 16:00:10 -0300

    add login page styles wp

--- src/components/googleSignIn/LogOut.js
+++ src/components/googleSignIn/LogOut.js
@@ -5,11 +5,10 @@ import { IoMdExit } from 'react-icons/io';
 
 
 function LogOut() {
+    //limpia el cache con el nombre de usuario obtenido de google y te envia a la pagina para registrarte
     const handleClick = () => {
         localStorage.clear()
-        window.location.href = `/asistentes`;
-
-
+        window.location.href = `/login`;
     }
     return (
         <button onClick={handleClick}>
--- src/components/googleSignIn/Login.js
+++ src/components/googleSignIn/Login.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { auth, provider } from './config'
 import { signInWithPopup } from 'firebase/auth';
+import '../styles/login.css';
+
 
 
 function Login() {
@@ -9,14 +11,25 @@ function Login() {
         signInWithPopup(auth, provider).then((data) => {
             setValue(data.user.displayName)
             localStorage.setItem("name", data.user.displayName)
+            window.location.href = `/capacitacion`;
         })
     }
     useEffect(() => {
         setValue(localStorage.getItem('name'))
     })
     return (
-        <div>
-            <button onClick={handleClick}>Ingresar con Google</button>
+        <div className='loginBackground'>
+            <div className='title'>
+                <h1 className='mainTittle'>Bienvenido a Mentor</h1>
+                <p className='mainTittle'>Sistema de capacitaciones</p>
+            </div>
+            <section className='loginSection'>
+                <div>
+                    <h1 className='mainTittle'>Iniciar Sesión</h1>
+                    <button className='buttonLogin' onClick={handleClick}>Ingresar con Google</button>
+                </div>
+
+            </section>
         </div>
     );
 };
